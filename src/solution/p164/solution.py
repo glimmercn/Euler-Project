ecf6184a7503796a8f21d28cb8d9abc3f1d76aa6
kanhuang <huangkandiy@gmail.com>
2014-03-15 16:56:17 -0400

    change a variable name.

--- src/solution/p164/solution.py
+++ src/solution/p164/solution.py
@@ -21,9 +21,9 @@ for i in range(18):
                 TempD[str(j)+k[0]] += D[k]
     D = TempD
 
-sum = 0
+Sum = 0
 for k in D.keys():
-    if k[0] != '0': sum += D[k] 
+    if k[0] != '0': Sum += D[k] 
 
-print(sum)
+print(Sum)
         
